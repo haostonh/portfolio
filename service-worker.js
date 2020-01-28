b6e848c7fb441a7768509acb32e842ed02801164
Haoston Huynh <hsthuynh@ucdavis.edu>
2020-01-27 22:26:14 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3f40d3936729d66ca8910e7cfb2569fe.js"
+  "/portfolio/precache-manifest.1caa105cd992fc112a95b2f7935b5d49.js"
 );
 
 self.addEventListener('message', (event) => {

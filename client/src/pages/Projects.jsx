7a3d8673b451cb899332675265df53cea3e70958
haostonh <hsthuynh@ucdavis.edu>
2022-03-22 15:26:46 -0700

    added banking system to project section

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -10,7 +10,16 @@ const Projects = () => {
         <style>{'body { background-color: #D3D3D3 }'}</style>
       </Helmet>
       <NavBar/>
-      <h1>Banking System</h1>
+      <div>
+        <h1 className='yearTitle'>2022</h1>
+        <h2 className='projectTitle'>Banking System (Jan 2022 - March 2022)</h2>
+        <ul className='projectBody'>
+          <li>
+            <a href='https://github.com/haostonh/Banking-System' target='_blank' rel='noreferrer'>GitHub Link</a>
+          </li>
+        </ul>
+
+      </div>
     </div>
   );
 };

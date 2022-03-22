c7ff26e2d816786c7806425757c76466ddbda02f
haostonh <hsthuynh@ucdavis.edu>
2022-03-22 15:53:12 -0700

    add technologies used for banking system

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -18,6 +18,15 @@ const Projects = () => {
             <a href='https://github.com/haostonh/Banking-System' target='_blank' rel='noreferrer'>GitHub Link</a>
           </li>
           <li>Created with Visual Studio 2019 and MySQL</li>
+          <li>Keep track of customer's balances using MySQL database</li>
+          <li>Technologies Used:</li>
+            <ul>
+              <li>C++</li>
+              <li>Visual Studio 2019 (with Desktop development with C++)</li>
+              <li>MySQL C++ Driver (Connector/C++)</li>
+              <li>MySQL for Visual Studio</li>
+              <li>MySQL Router/Server</li>
+            </ul>
         </ul>
 
       </div>

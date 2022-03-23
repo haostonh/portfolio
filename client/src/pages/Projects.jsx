ad04ecae6bf928c538599dc783a04abb512212cb
haostonh <hsthuynh@ucdavis.edu>
2022-03-23 13:14:58 -0700

    added second project to page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -12,14 +12,14 @@ const Projects = () => {
       <NavBar/>
       <div className='projectsFont'>
         <h1 className='yearTitle'>2022</h1>
-        <div>
+        <div className='projectsProjectContainer'>
           <h2 className='projectTitle'>Banking System (Jan 2022 - Mar 2022)</h2>
           <ul className='projectBody'>
             <li>
               <a href='https://github.com/haostonh/Banking-System' target='_blank' rel='noreferrer'>GitHub Link</a>
             </li>
             <li>Created with Visual Studio 2019 and MySQL</li>
-            <li>Keep track of customer's balances using MySQL database</li>
+            <li>Keep Track of Customer's Balances using MySQL database</li>
             <li>Technologies Used:</li>
               <ul>
                 <li>C++</li>
@@ -27,6 +27,21 @@ const Projects = () => {
                 <li>MySQL C++ Driver (Connector/C++)</li>
                 <li>MySQL for Visual Studio</li>
                 <li>MySQL Router/Server</li>
+              </ul>
+          </ul>
+        </div>
+        <div>
+          <h2 className='projectTitle'>Tic-Tac-Toe (Jan 2022 - Mar 2022)</h2>
+          <ul className='projectBody'>
+            <li>
+              <a href='https://github.com/haostonh/tic-tac-toe' target='_blank' rel='noreferrer'>GitHub Link</a>
+            </li>
+            <li>Created with C++</li>
+            <li>Simple Game of Tic-Tac-Toe</li>
+            <li>Code Compilation with Makefile</li>
+            <li>Technologies Used:</li>
+              <ul>
+                <li>C++</li>
               </ul>
           </ul>
         </div>

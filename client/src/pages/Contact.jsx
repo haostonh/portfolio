602c6363ff244f393cb8022b7fc3270db3a2800d
haostonh <hsthuynh@ucdavis.edu>
2022-03-24 15:51:14 -0700

    add github profile link and icon

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import '../pagesCSS/Contact.css';
 import NavBar from '../components/NavBar';
 import linkedinicon from '../images/linkedin-icon.jpg';
+import githubicon from '../images/GitHub-Mark-Light-64px.png'
 
 const Contact = () => {
   return (
@@ -14,8 +15,12 @@ const Contact = () => {
       <div className='contactFont'>
         <h1 className='contactTitle'>GET IN TOUCH</h1>
         <div className='contactPictureandTextBox'>
-          <img className='contactPictureFormat' src={linkedinicon} alt={"LinkedIn Icon"} width='50' height='50'/>
+          <img className='contactPictureFormat' src={linkedinicon} alt={"LinkedIn Icon"} width='64' height='64'/>
           <a className='contactTextFormat' href='https://www.linkedin.com/in/haoston-huynh/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
+        </div>
+        <div className='contactPictureandTextBox'>
+          <img className='contactPictureFormat' src={githubicon} alt={"GitHub Icon"} width='64' height='64'/>
+          <a className='contactTextFormat' href='https://github.com/haostonh' target='_blank' rel='noopener noreferrer'>GitHub</a>
         </div>
       </div>
     </div>

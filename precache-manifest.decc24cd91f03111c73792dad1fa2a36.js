e8bf8287d6278839bcb5f0d6199e7649e56b7ac1
haostonh <haostonhuynh711@gmail.com>
2022-03-24 15:15:21 -0700

    Updates

---
+++ precache-manifest.decc24cd91f03111c73792dad1fa2a36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3a06730fb6ef2d148b6",
+    "url": "/portfolio/static/css/main.5edc0f5b.chunk.css"
+  },
+  {
+    "revision": "d3a06730fb6ef2d148b6",
+    "url": "/portfolio/static/js/main.5bb3cc99.chunk.js"
+  },
+  {
+    "revision": "d2097018179dbdbc6f96",
+    "url": "/portfolio/static/js/runtime~main.2f157621.js"
+  },
+  {
+    "revision": "b1c776aa589288702d5c",
+    "url": "/portfolio/static/js/2.3d34dcdd.chunk.js"
+  },
+  {
+    "revision": "4c7a19e055998208c1c3",
+    "url": "/portfolio/static/js/3.43ac7eac.chunk.js"
+  },
+  {
+    "revision": "2d87ac3e5fb772fea3fc754eb409dc8a",
+    "url": "/portfolio/index.html"
+  }
+];

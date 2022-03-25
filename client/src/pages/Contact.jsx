4b21e406ad94e49efab9743f8f9a075e41dce6c3
haostonh <hsthuynh@ucdavis.edu>
2022-03-25 15:39:11 -0700

    add hackerrank profile on contact page

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -3,7 +3,8 @@ import Helmet from 'react-helmet';
 import '../pagesCSS/Contact.css';
 import NavBar from '../components/NavBar';
 import linkedinicon from '../images/linkedin-icon.jpg';
-import githubicon from '../images/GitHub-Mark-64px.png'
+import githubicon from '../images/GitHub-Mark-64px.png';
+import hackerrankicon from '../images/HackerRank-Icon-480px.png';
 
 const Contact = () => {
   return (
@@ -21,6 +22,10 @@ const Contact = () => {
         <div className='contactPictureandTextBox'>
           <img className='contactPictureFormat' src={githubicon} alt={"GitHub Icon"} width='64' height='64'/>
           <a className='contactTextFormat' href='https://github.com/haostonh' target='_blank' rel='noopener noreferrer'>GitHub</a>
+        </div>
+        <div className='contactPictureandTextBox'>
+          <img className='contactPictureFormat' src={hackerrankicon} alt={"HackerRank Icon"} width='64' height='64'/>
+          <a className='contactTextFormat' href='https://www.hackerrank.com/haostonh' target='_blank' rel='noopener noreferrer'>HackerRank</a>
         </div>
       </div>
     </div>

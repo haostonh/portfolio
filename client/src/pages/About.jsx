c19989e766ba073ec08fcc2e637e9f7c804aa86a
haostonh <hsthuynh@ucdavis.edu>
2022-03-25 22:51:28 -0700

    testing pictures on website

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import '../pagesCSS/About.css';
 import NavBar from '../components/NavBar';
+import resumelogo from '../images/HH-Logo-Transparent-576px.png';
 
 const About = () => {
   return (
@@ -10,6 +11,7 @@ const About = () => {
         <style>{'body { background-color: #FFFFF0 }'}</style>
       </Helmet>
       <NavBar/>
+      <img src={resumelogo} alt={"Haoston Huynh's Logo"}/>
     </div>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import '../pagesCSS/HomePage.css';
 import NavBar from '../components/NavBar';
+import profile from '../images/Haoston_Huynh_Profile_Picture.jpg';
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
         <style>{'body { background-color: #FFFFF0 }'}</style>
       </Helmet>
       <NavBar/>
+      <img src={profile} alt={"Haoston Huynh's Profile"} width='256' height='384'/>
     </div>
   );
 };

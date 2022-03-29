24c2249e2fc777a4abd4a31a4bd1aed19a4791fb
haostonh <hsthuynh@ucdavis.edu>
2022-03-29 15:08:04 -0700

    change class to id

--- client/src/components/IconAndTextBox.jsx
+++ client/src/components/IconAndTextBox.jsx
@@ -3,9 +3,9 @@ import '../componentsCSS/IconAndTextBox.css';
 
 const IconAndTextBox = props => {
   return (
-    <div className='componentIconTextBox'>
-      <img className='componentIconFormat' src={props.src} alt={props.alt}/>
-      <a className='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'>{props.text}</a>
+    <div id='componentIconTextBox'>
+      <img id='componentIconFormat' src={props.src} alt={props.alt}/>
+      <a id='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'>{props.text}</a>
     </div>
   )
 }

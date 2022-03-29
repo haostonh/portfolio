babbe7aa4c722496c5038407d7dddb6596c4bb21
haostonh <hsthuynh@ucdavis.edu>
2022-03-28 21:04:08 -0700

    use async helmet for UNSAFE_component...

--- client/src/components/PortfolioHelmet.jsx
+++ client/src/components/PortfolioHelmet.jsx
@@ -1,1 +1,14 @@
 import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+const PortfolioHelmet = () => {
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <style>{'body { background-color: #FFFFF0 }'}</style>
+      </Helmet>
+    </HelmetProvider>
+  )
+}
+
+export default PortfolioHelmet;

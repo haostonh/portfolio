6312c475f51f19c5b28446b14aec10a34bc71eaf
haostonh <hsthuynh@ucdavis.edu>
2022-03-28 22:17:26 -0700

    remove commented out picture and text boxes

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -19,22 +19,6 @@ const Contact = () => {
         <IconAndTextBox src={githubicon} alt={'GitHub Icon'} text='GitHub' href='https://github.com/haostonh'/>
         <IconAndTextBox src={leetcodeicon} alt={'LeetCode Icon'} text='LeetCode' href='https://leetcode.com/haostonh/'/>
         <IconAndTextBox src={hackerrankicon} alt={'HackerRank Icon'} text='HackerRank' href='https://www.hackerrank.com/haostonh'/>
-        {/*<div className='contactPictureandTextBox'>
-          <img className='contactPictureFormat' src={linkedinicon} alt={"LinkedIn Icon"}/>
-          <a className='contactTextFormat' href='https://www.linkedin.com/in/haoston-huynh/' target='_blank' rel='noopener noreferrer'>LinkedIn</a>
-        </div>
-        <div className='contactPictureandTextBox'>
-          <img className='contactPictureFormat' src={githubicon} alt={"GitHub Icon"}/>
-          <a className='contactTextFormat' href='https://github.com/haostonh' target='_blank' rel='noopener noreferrer'>GitHub</a>
-        </div>
-        <div className='contactPictureandTextBox'>
-          <img className='contactPictureFormat' src={leetcodeicon} alt={"LeetCode Icon"}/>
-          <a className='contactTextFormat' href='https://leetcode.com/haostonh/' target='_blank' rel='noopener noreferrer'>LeetCode</a>
-        </div>
-        <div className='contactPictureandTextBox'>
-          <img className='contactPictureFormat' src={hackerrankicon} alt={"HackerRank Icon"}/>
-          <a className='contactTextFormat' href='https://www.hackerrank.com/haostonh' target='_blank' rel='noopener noreferrer'>HackerRank</a>
-        </div>*/}
       </div>
     </div>
   );

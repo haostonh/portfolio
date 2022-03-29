1aa04f905d21279f6c3f02f876879b00dd2b848d
haostonh <hsthuynh@ucdavis.edu>
2022-03-28 22:31:16 -0700

    change to single quotes pt.2

--- client/src/components/NavBarButton.jsx
+++ client/src/components/NavBarButton.jsx
@@ -4,21 +4,21 @@ import Button from '@mui/material/Button';
 
 const NavBarButton = props => {
   return (
-    <Button component={Link} to={props.to} color="primary"
+    <Button component={Link} to={props.to} color='primary'
     sx={{
-      color: "#000000",
+      color: '#000000',
       fontFamily: "'Roboto', sans-serif", 
-      fontSize: "25px",
-      fontWeight: "500", 
-      marginLeft: "5px",
-      marginRight: "5px",
+      fontSize: '25px',
+      fontWeight: '500', 
+      marginLeft: '5px',
+      marginRight: '5px',
       '@media (min-width: 426px) and (max-width: 1024px)': {
-        fontSize: "25px",
+        fontSize: '25px',
       },
       '@media (max-width: 425px)': {
-        fontSize: "15px",
-        marginLeft: "0px",
-        marginRight: "0px",
+        fontSize: '15px',
+        marginLeft: '0px',
+        marginRight: '0px',
       },
     }}>{props.text}</Button>
   )

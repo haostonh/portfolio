b5d94107cbf1888aebbcafd4a00f8c12197b31a1
haostonh <hsthuynh@ucdavis.edu>
2022-03-30 15:01:06 -0700

    adding hobbies/interest section

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,6 +9,9 @@ const About = () => {
       <PortfolioHelmet/>
       <NavBar/>
       <h1 className='aboutHeaderTitle'>Education</h1>
+      <div>Computer Science B.S. from University of California, Davis (2017-2021)</div>
+      <div>Independence High School (2013-2017)</div>
+      <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
     </div>
   );
 };

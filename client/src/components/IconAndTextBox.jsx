b097ee6af43b692fad31bcb7603a3208f567f904
haostonh <hsthuynh@ucdavis.edu>
2022-04-04 16:28:34 -0700

    add padding left/right of contact buttons

--- client/src/components/IconAndTextBox.jsx
+++ client/src/components/IconAndTextBox.jsx
@@ -6,7 +6,11 @@ const IconAndTextBox = props => {
   return (
     <div id='componentIconTextBox'>
       <img id='componentIconFormat' src={props.src} alt={props.alt}/>
-      <Button id='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'>{props.text}</Button>
+      <Button id='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'
+        sx={{
+          paddingLeft: '15px',
+          paddingRight: '15px',
+        }}>{props.text}</Button>
     </div>
   )
 }

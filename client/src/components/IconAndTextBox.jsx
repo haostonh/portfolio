6425387cf79bd34892ba1e75221e35ed620684d7
haostonh <hsthuynh@ucdavis.edu>
2022-04-04 16:14:57 -0700

    change to button component

--- client/src/components/IconAndTextBox.jsx
+++ client/src/components/IconAndTextBox.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import '../componentsCSS/IconAndTextBox.css';
+import Button from '@mui/material/Button';
 
 const IconAndTextBox = props => {
   return (
     <div id='componentIconTextBox'>
       <img id='componentIconFormat' src={props.src} alt={props.alt}/>
-      <a id='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'>{props.text}</a>
+      <Button id='componentTextFormat' href={props.href} target='_blank' rel='noopener noreferrer'>{props.text}</Button>
     </div>
   )
 }

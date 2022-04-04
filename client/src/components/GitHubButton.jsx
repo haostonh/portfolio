9da2b9804f5ea4c1e1bc0f27d063f52649c7c946
haostonh <hsthuynh@ucdavis.edu>
2022-04-04 15:37:51 -0700

    create github button

--- client/src/components/GitHubButton.jsx
+++ client/src/components/GitHubButton.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const GitHubButton = () => {
   return (
-
+    <Button>GitHub</Button>
   )
 }
 

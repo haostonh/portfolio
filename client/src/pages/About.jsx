e75ade001ed7c2660171f11abf0d0fa5e7052036
haostonh <hsthuynh@ucdavis.edu>
2022-04-04 14:25:38 -0700

    add css to body section of education

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -9,8 +9,8 @@ const About = () => {
       <PortfolioHelmet/>
       <NavBar/>
       <h1 className='aboutHeaderTitle'>Education</h1>
-      <div>Computer Science B.S. from University of California, Davis (2017-2021)</div>
-      <div>Independence High School (2013-2017)</div>
+      <div className='aboutBody'>Computer Science B.S. from University of California, Davis (2017-2021)</div>
+      <div className='aboutBody'>Independence High School (2013-2017)</div>
       <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
     </div>
   );

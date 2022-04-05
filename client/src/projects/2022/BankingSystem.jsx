c0d99cbf6a9f547e0e3292bcb406bfccea8c876a
haostonh <hsthuynh@ucdavis.edu>
2022-04-04 17:02:38 -0700

    updated button in banking system project

--- client/src/projects/2022/BankingSystem.jsx
+++ client/src/projects/2022/BankingSystem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import '../../pagesCSS/Projects.css';
+import HHLinkButton from "../../components/HHLinkButton";
 
 const BankingSystem = () => {
   return (
@@ -7,7 +8,8 @@ const BankingSystem = () => {
       <h2 className='projectTitle'>Banking System (Jan 2022 - Mar 2022)</h2>
       <ul className='projectBody'>
         <li>
-          <a href='https://github.com/haostonh/Banking-System' target='_blank' rel='noopener noreferrer'>GitHub Link</a>
+          <HHLinkButton href='https://github.com/haostonh/Banking-System' text='GitHub'/>
+          
         </li>
         <li>Created with Visual Studio 2019 and MySQL</li>
         <li>Keep Track of Customer's Balances using MySQL database</li>

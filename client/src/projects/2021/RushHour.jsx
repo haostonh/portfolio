ba7c72aa32bf4061710b031959fb31d5f69b8c1d
haostonh <hsthuynh@ucdavis.edu>
2022-04-06 17:51:42 -0700

    create RushHour project

---
+++ client/src/projects/2021/RushHour.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import '../../pagesCSS/Projects.css';
+
+const RushHour = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>Rush Hour (Apr 2021 - May 2021)</h2>
+      <ul className='projectBody'>
+        <li>Created with Python 3</li>
+        <li>Technologies Used:</li>
+          <ul>
+            <li>Python 3</li>
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default RushHour;

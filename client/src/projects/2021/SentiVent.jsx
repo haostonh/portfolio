600107b816b56b78f385b995a221e82830757209
haostonh <hsthuynh@ucdavis.edu>
2022-04-07 16:59:09 -0700

    Added link to SentiVent

--- client/src/projects/2021/SentiVent.jsx
+++ client/src/projects/2021/SentiVent.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import '../../pagesCSS/Projects.css';
+import HHLinkButton from "../../components/HHLinkButton";
 
 const SentiVent = () => {
   return (
     <div className='projectInfo'>
       <h2 className='projectTitle'>SentiVent (Jan 2021)</h2>
       <ul className='projectBody'>
+        <li>
+          <HHLinkButton href='https://devpost.com/software/sentivent-084ypx' text='Devpost'/>
+        </li>
         <li>Write journal entries to vent emotions</li>
         <li>Track Keywords using Cloud Natural Language API</li>
         <li>Technologies Used:</li>

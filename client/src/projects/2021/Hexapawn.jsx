0d6a1efed33384dcea16d332ec15d773d4d0e12b
haostonh <hsthuynh@ucdavis.edu>
2022-04-07 14:47:14 -0700

    fix spelling for hexapawn

--- client/src/projects/2021/Hexapawn.jsx
+++ client/src/projects/2021/Hexapawn.jsx
@@ -7,8 +7,8 @@ const Hexapawn = () => {
       <h2 className='projectTitle'>Hexapawn (May 2021)</h2>
       <ul className='projectBody'>
         <li>3 X 3 Chess Board</li>
-        <li>3 pawns per side of chess board</li>
-        <li>First chess piece to reach other side wins</li>
+        <li>3 pawns per side of the chess board</li>
+        <li>First chess piece to reach the other side wins</li>
         <li>Follows movement and capture of regular pawns</li>
         <li>Technologies Used:</li>
           <ul>

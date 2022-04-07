b14445b18c4a82fa59038f3e2566ff322a4b548f
haostonh <hsthuynh@ucdavis.edu>
2022-04-07 16:01:17 -0700

    Template for SentiVent Project

---
+++ client/src/projects/2021/SentiVent.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../pagesCSS/Projects.css';
+
+const SentiVent = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>SentiVent (Jan 2021)</h2>
+      <ul className='projectBody'>
+        <li>Technologies Used:</li>
+          <ul>
+            
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default SentiVent

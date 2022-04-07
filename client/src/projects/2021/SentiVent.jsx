72ad24e040e098777bb7d2065a9443bdae898d0b
haostonh <hsthuynh@ucdavis.edu>
2022-04-07 16:29:18 -0700

    SentiVent description

--- client/src/projects/2021/SentiVent.jsx
+++ client/src/projects/2021/SentiVent.jsx
@@ -6,9 +6,11 @@ const SentiVent = () => {
     <div className='projectInfo'>
       <h2 className='projectTitle'>SentiVent (Jan 2021)</h2>
       <ul className='projectBody'>
+        <li>Write journal entries to vent emotions</li>
+        <li>Track Keywords using Cloud Natural Language API</li>
         <li>Technologies Used:</li>
           <ul>
-            <li>React</li>
+            <li>React Native</li>
             <li>ExpressJS</li>
             <li>Google Cloud API</li>
             <li>OAuth</li>

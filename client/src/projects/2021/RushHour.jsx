abd0b158af4bd61b974ae7b2a572fbad04cf6e9c
haostonh <hsthuynh@ucdavis.edu>
2022-04-06 17:55:58 -0700

    update rushhour description

--- client/src/projects/2021/RushHour.jsx
+++ client/src/projects/2021/RushHour.jsx
@@ -9,7 +9,8 @@ const RushHour = () => {
         <li>Created with Python 3</li>
         <li>Vehicle exits a traffic jam/grid in optimal number of moves</li>
         <li>Created 2 heuristics comparing runtimes and h(n)</li>
-        <li>Implemented using the A* search algorithm</li>
+        <li>First heuristic: Implemented using the A* search algorithm</li>
+        <li>Second heruristic: Created my own</li>
         <li>Technologies Used:</li>
           <ul>
             <li>Python 3</li>

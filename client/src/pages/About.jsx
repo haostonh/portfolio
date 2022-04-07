186ba45291a80f561b02c8dba78a36df24347151
haostonh <hsthuynh@ucdavis.edu>
2022-04-06 17:23:17 -0700

    change styling to a lower div

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,13 +5,15 @@ import PortfolioHelmet from '../components/PortfolioHelmet';
 
 const About = () => {
   return (
-    <div id='aboutFont'>
+    <div>
       <PortfolioHelmet/>
       <NavBar/>
-      <h1 className='aboutHeaderTitle'>Education</h1>
-      <div className='aboutBody'>University of California, Davis (2017 - 2021)</div>
-      <div className='aboutBody'>Independence High School (2013 - 2017)</div>
-      <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
+      <div id='aboutFont'>
+        <h1 className='aboutHeaderTitle'>Education</h1>
+        <div className='aboutBody'>University of California, Davis (2017 - 2021)</div>
+        <div className='aboutBody'>Independence High School (2013 - 2017)</div>
+        <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
+      </div>
     </div>
   );
 };

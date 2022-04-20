d7eb506e77433f0984423286229ce55a62d3b509
haostonh <hsthuynh@ucdavis.edu>
2022-04-19 20:48:28 -0700

    added hobbies and interests

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -13,6 +13,9 @@ const About = () => {
         <div className='aboutBody'>University of California, Davis (2017 - 2021)</div>
         <div className='aboutBody'>Independence High School (2013 - 2017)</div>
         <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
+        <div className='aboutBody'>Orienteering</div>
+        <div className='aboutBody'>Piano</div>
+        <div className='aboutBody'>Wallyball</div>
       </div>
     </div>
   );

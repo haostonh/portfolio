ebc80f70b6f841b1f00871abe2f7106541fba0e7
haostonh <hsthuynh@ucdavis.edu>
2022-04-20 14:56:11 -0700

    RSS Reader Description

---
+++ client/src/projects/2021/RSSReader.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../../pagesCSS/Projects.css';
+
+const RSSReader = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>RSS Reader (Jan 2021 - Feb 2021)</h2>
+      <ul className='projectBody'>
+        <li>Web feed with multiple categories</li>
+        <li>Displays current events as they occur</li>
+        <li>Simple and easy to use interface</li>
+        <li>Practice building layouts, activities, and activity adapters</li>
+        <li>Working with Intents</li>
+        <li>Technologies Used:</li>
+          <ul>
+            <li>Kotlin</li>
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default Hexapawn;

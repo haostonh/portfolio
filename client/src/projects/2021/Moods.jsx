b123f36a7fae52ae2a209ee4b8962f53e08fcdbf
haostonh <hsthuynh@ucdavis.edu>
2022-04-25 16:02:13 -0700

    created Moods jsx

---
+++ client/src/projects/2021/Moods.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import '../../pagesCSS/Projects.css';
+
+const Moods = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>Moods (Feb 2021)</h2>
+      <ul className='projectBody'>
+        <li>Technologies Used:</li>
+          <ul>
+            <li>Kotlin</li>
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default Moods;

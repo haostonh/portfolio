0271b8b583a962933e82a46332c3288fa2f915d2
haostonh <hsthuynh@ucdavis.edu>
2022-04-25 16:06:05 -0700

    Updated description of Moods

--- client/src/projects/2021/Moods.jsx
+++ client/src/projects/2021/Moods.jsx
@@ -6,6 +6,9 @@ const Moods = () => {
     <div className='projectInfo'>
       <h2 className='projectTitle'>Moods (Feb 2021)</h2>
       <ul className='projectBody'>
+        <li>Allow user to create notes to reflect their mood</li>
+        <li>Practice using Jetpack Navigation</li>
+        <li>Used Room Database to store data</li>
         <li>Technologies Used:</li>
           <ul>
             <li>Kotlin</li>

035e2d742a50c580a62f821e9021791a265c8da5
haostonh <hsthuynh@ucdavis.edu>
2022-04-25 16:55:46 -0700

    Create RentWell Project

---
+++ client/src/projects/2021/RentWell.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import '../../pagesCSS/Projects.css';
+
+const RentWell = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>RentWell (Mar 2021)</h2>
+      <ul className='projectBody'>
+        <li>Allow user to post their apartment listing</li>
+        <li>View apartment listings with email button as a point of contact</li>
+        <li>Created services for Reset API Endpoints</li>
+        <li>Used Firebase Authentication</li>
+        <li>Practice Jetpack Navigation</li>
+        <li>Technologies Used:</li>
+          <ul>
+            <li>Kotlin</li>
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default RentWell;

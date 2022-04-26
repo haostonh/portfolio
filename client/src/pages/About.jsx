4d90f79f3a64c2fabda07d2df67adfa2b7720a50
haostonh <hsthuynh@ucdavis.edu>
2022-04-26 16:21:29 -0700

    add item container for About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -11,8 +11,10 @@ const About = () => {
       <NavBar/>
       <div id='aboutPageStyle'>
         <h1 className='aboutHeaderTitle'>Education</h1>
-        <img id='UCDavisLogo' src={UCDavisLogo} alt='UC Davis Logo' width='175px'/>
-        <div className='aboutBody'>University of California, Davis (2017 - 2021)</div>
+        <div className='aboutItemContainer'>
+          <img id='UCDavisLogo' src={UCDavisLogo} alt='UC Davis Logo' width='175px'/>
+          <div className='aboutBody'>University of California, Davis (2017 - 2021)</div>
+        </div>
         <div className='aboutBody'>Independence High School (2013 - 2017)</div>
         <h1 className='aboutHeaderTitle'>Hobbies/Interests</h1>
         <div className='aboutBody'>Orienteering</div>

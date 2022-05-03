07e6128766cc03c9177b9dfc2b8ba980b1bc0efc
haostonh <hsthuynh@ucdavis.edu>
2022-05-02 23:14:36 -0700

    create SongWithFriends Description

---
+++ client/src/projects/2020/SongWithFriends.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import HHLinkButton from "../../components/HHLinkButton";
+import '../../pagesCSS/Projects.css';
+
+const SongWithFriends = () => {
+  return (
+    <div className='projectInfo'>
+      <h2 className='projectTitle'>Song With Friends (May 2020 - Jun 2020)</h2>
+      <ul className='projectBody'>
+        <li></li>
+        <li>Technologies Used:</li>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>React</li>
+            <li>Redux</li>
+          </ul>
+      </ul>
+    </div>
+  )
+}
+
+export default SongWithFriends;
